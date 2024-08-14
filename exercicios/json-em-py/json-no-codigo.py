18d344a65c6d6211a900c7a8324693b57c59876b
wallace <w.wallacebarros.cruz@gmail.com>
2024-08-13 21:44:10 -0300

    adicionando e modificando valores de um arquivo json

--- exercicios/json-em-py/json-no-codigo.py
+++ exercicios/json-em-py/json-no-codigo.py
@@ -1,6 +1,9 @@
 import json
 
-with open("dados.json", "r") as target:
-    data = json.load(target)
+with open("dados.json", "r") as file:
+    data = json.load(file)
 
-print(data["nome"])
+data["nome"] = "wallace barros"
+
+with open("dados.json", "w") as file:
+    json.dump(data, file)
